b6926959b241946f16481924f646fd477fa0c246
Michael Sowah <michael.t.sowah@gmail.com>
2026-05-20 20:26:49 +0000

    test(orm): add ResetsLazyLoading trait for N+1 detector state isolation

---
+++ tests/Support/Traits/ResetsLazyLoading.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Glueful\Tests\Support\Traits;
+
+use Glueful\Database\ORM\Model;
+
+/**
+ * PHPUnit trait that clears N+1 detector static state in tearDown().
+ * Include this in any test class that mutates lazy-loading global state.
+ */
+trait ResetsLazyLoading
+{
+    protected function tearDown(): void
+    {
+        if (method_exists(Model::class, 'resetLazyLoadingState')) {
+            Model::resetLazyLoadingState();
+        }
+        parent::tearDown();
+    }
+}
